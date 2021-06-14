c0462ac0b97f63bc9e0abbca630949c26a9d3649
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-06-15 03:24:58 +0530

    Do one more task using groupby

--- group_ops.py
+++ group_ops.py
@@ -12,15 +12,37 @@ from indexing_filtering import read_tate_dataset
 
 def titles_per_acq_year(df: pd.DataFrame):
     df_groupby_acq_year = df.groupby(['acquisitionYear'])
-    print(df_groupby_acq_year)
 
+    # Method 1: Using Generics
     for name, group_df in df_groupby_acq_year:
-        print(f"Titles acquired in year {name}: {len(group_df)}")
+        print(f"Titles acquired in year {int(name)}: {len(group_df)}")
+
+    print()
+    # Method 2A: Using pandas Builtin 'agg' method
+    df = df_groupby_acq_year.agg('count').loc[:, 'title']
+    print(df)
+
+    print()
+    # Method 2B: Using pandas Builtin 'agg' method's alternate
+    df = df_groupby_acq_year.count().loc[:, 'title']
+    print(df)
+
+
+def oldest_artwork_per_artist(df: pd.DataFrame):
+    df_groupby_artist = df.groupby(['artist'])
+
+    for name, group_df in df_groupby_artist:
+        print(f"The first artwork from {name} was acquired in {int(group_df.loc[:, 'acquisitionYear'].min())}")
 
 
 if __name__ == '__main__':
     tate_dataset_df = read_tate_dataset()
+    tate_dataset_df_subset = tate_dataset_df.iloc[30000:32000, :]
 
     print("Task 1: Total Titles acquired per year")
-    titles_per_acq_year(tate_dataset_df)
+    titles_per_acq_year(tate_dataset_df_subset)
+    print()
+
+    print("Task 2: Total Titles acquired per year")
+    oldest_artwork_per_artist(tate_dataset_df_subset)
     print()
