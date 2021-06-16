e10c979d9e684a5fb1a54fa7430266052e8388f2
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-06-16 23:36:22 +0530

    Add example for 'transform' operation

--- group_ops.py
+++ group_ops.py
@@ -6,6 +6,8 @@
     3. Filtering: Removing rows with unknown 'title' OR
                   Dropping the rows with unique titles (i.e. keeping only duplicated titles)
 """
+from typing import Optional
+
 import pandas as pd
 from indexing_filtering import read_tate_dataset
 
@@ -57,15 +59,40 @@ def oldest_artwork_per_artist(df: pd.DataFrame):
     print(s3)
 
 
+def _find_most_freq_medium(series: pd.Series) -> Optional[str]:
+    counts = series.value_counts()      # type: pd.Series
+    if counts.min() == counts.max():
+        return None
+    return counts.idxmax()
+
+
+def replace_unknown_mediums(df: pd.DataFrame):
+    mediums_groupby_artist = df.groupby(['artist'])['medium']
+
+    # Method 1
+    for name, group_series in mediums_groupby_artist:   # type: str, pd.Series
+        cnt_mediums = len(group_series)
+        if cnt_mediums > 0:
+            print(group_series)
+            medium = _find_most_freq_medium(group_series)
+            if medium:
+                df.fillna(value={'medium': medium}, inplace=True)
+
+
+
+
 if __name__ == '__main__':
     tate_dataset_df = read_tate_dataset()
-    tate_dataset_df_subset = tate_dataset_df.iloc[49990:50140, :]
-    print(tate_dataset_df_subset)
+    tate_dataset_df_subset = tate_dataset_df.iloc[49900:50100, :]
 
-    print("Task 1: Total Titles acquired per year:")
-    titles_per_acq_year(tate_dataset_df_subset)
-    print()
+    # print("Task 1: Total Titles acquired per year:")
+    # titles_per_acq_year(tate_dataset_df_subset)
+    # print()
+    #
+    # print("Task 2: Oldest artwork per artist")
+    # oldest_artwork_per_artist(tate_dataset_df_subset)
+    # print()
 
-    print("Task 2: Oldest artwork per artist")
-    oldest_artwork_per_artist(tate_dataset_df_subset)
+    print("Task 3: Unknown mediums replaced with most used by that artist")
+    replace_unknown_mediums(tate_dataset_df)
     print()
